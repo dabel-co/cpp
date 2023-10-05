f7a5991736020690e63e8a35372769e3156d0bd6
dabel-co <dabel-co@student.42madrid.com>
2023-10-05 14:47:37 +0200

    c01 up

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-void replaceAll(std::string& str, const std::string& from, const std::string& to)
+void replace(std::string& str, const std::string& from, const std::string& to)
 {
     size_t startPos = 0;
     size_t foundPos;
@@ -11,32 +11,30 @@ void replaceAll(std::string& str, const std::string& from, const std::string& to
         startPos = foundPos + to.length();
     }
 }
-//change order and stuff
+
 int main(int argc, char **argv)
 {
+    if (argc != 4 || !argv[1] || !argv[2] || !argv[3])
+    {
+        std::cout << "Error" << std::endl;
+        return (1);
+    }
     std::string inputname = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
     std::ifstream   inputfile(inputname);
+    if (inputfile.fail())
+    {
+        std::cout << "Error, file not found" << std::endl;
+        return (1);
+    }
     std::ofstream   output(inputname + ".replace");
     std::string     line;
-    std::cout << inputname << std::endl;
-
-    if (argc == 4)
+    
+    while (std::getline(inputfile, line))
     {
-        while (std::getline(inputfile, line))
-        {
-            replaceAll(line, argv[2], argv[3]);
-            output << line << '\n';
-        }
-        inputfile.close();
-        output.close();
+        replace(line, argv[2], argv[3]);
+        output << line << '\n';
     }
-    else
-    {
-         std::cout << "error, usage : replace <file> old_word new_word" << std::endl;
-         return(1);
-    }
+    inputfile.close();
+    output.close();
     return(0);
-
 }
