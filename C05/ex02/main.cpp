78375f9392cb990a52843696cc6cd98f0f06ef55
dabel-co <dabel-co@student.42madrid.com>
2024-02-12 18:42:39 +0100

    added good make flags to C05 and stuff

--- C05/ex02/main.cpp
+++ C05/ex02/main.cpp
@@ -2,16 +2,23 @@
 #include "FormA.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main()
 {
     try 
     {
-        Bureaucrat pedro("Pedro de la cruz", 100);
-        ShrubberyCreationForm formulario("boring_form");
-        RobotomyRequestForm robotform("robotic");
-        pedro.signForm(robotform);
-        pedro.executeForm(robotform);
+        Bureaucrat pedro("Pedro de la cruz", 1);
+        ShrubberyCreationForm formulario1("boring_form");
+        RobotomyRequestForm formulario2("whatever");
+        PresidentialPardonForm formulario3("Ramon");
+        pedro.signForm(formulario1);
+        pedro.signForm(formulario2);
+        pedro.signForm(formulario3);
+        pedro.executeForm(formulario1);
+        pedro.executeForm(formulario2);
+        pedro.executeForm(formulario3);
+        //add all test
     }
     catch(std::exception & e)
     {
