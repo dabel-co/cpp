6754292372ffe3b8045847f4226cdaaa872ff577
David <david@14.local>
2024-05-14 17:35:07 +0200

    never been prouder of something i coded, need to add std::list and stuff

---
+++ C09/ex02/PmergeMe.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <list>
+#include <array>
+#include <unistd.h>
+#include <sys/time.h>
+
+void group_divide(const std::vector<int>& vector, std::vector<std::pair<int, int> >& pairs)
+{
+    for (size_t i = 0; i < vector.size() - 1; i += 2)
+	{
+        if (vector[i] > vector[i + 1])
+        	pairs.push_back(std::make_pair(vector[i + 1], vector[i])); // step 2, swap order if left element is bigger than right element
+        else
+        	pairs.push_back(std::make_pair(vector[i], vector[i + 1]));
+    }
+}
+
+void mergePairs(std::vector<std::pair<int, int> >& pairs, int low, int mid, int high)
+{
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    std::vector<std::pair<int, int> > left(n1), right(n2);
+    for (int i = 0; i < n1; i++)
+        left[i] = pairs[low + i];
+    for (int j = 0; j < n2; j++)
+        right[j] = pairs[mid + 1 + j];
+
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2)
+	{
+        if (left[i].second <= right[j].second)
+        	pairs[k++] = left[i++];
+        else
+        	pairs[k++] = right[j++];
+        
+    }
+    while (i < n1)
+        pairs[k++] = left[i++];
+    while (j < n2)
+        pairs[k++] = right[j++];
+}
+
+void recursiveSort(std::vector<std::pair<int, int> >& pairs, int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+
+        recursiveSort(pairs, low, mid);
+        recursiveSort(pairs, mid + 1, high);
+        mergePairs(pairs, low, mid, high);
+    }
+}
+
+void    binary_insert(std::vector<int>& mainChain, std::vector<int>& pend)
+{
+    size_t  currentPendIndex = 0, currentMainIndex = 0;
+    size_t  jacobsthal_diff[] = { //https://oeis.org/A078008
+        2, 2, 6, 10, 22, 42, 86, 170, 342, 682, 1366,
+        2730, 5462, 10922, 21846, 43690, 87382, 174762, 349526, 699050,
+        1398102, 2796202, 5592406, 11184810, 22369622, 44739242, 89478486,
+        178956970, 357913942, 715827882, 1431655766};
+    
+    for (int k = 0;; ++k)
+    {
+        if (jacobsthal_diff[k] > pend.size() - currentPendIndex)
+            break;
+        for (size_t offset = 0; offset < jacobsthal_diff[k]; ++offset)
+        {
+            int pendulumElement = pend[currentPendIndex + offset];
+            int insertPos = std::upper_bound(mainChain.begin(), mainChain.begin() + currentMainIndex + offset + 1, pendulumElement) - mainChain.begin(); //std::upper_bound is binary search https://eklitzke.org/std-upper-bound-and-std-lower-bound
+            mainChain.insert(mainChain.begin() + insertPos, pendulumElement);
+        }
+        currentPendIndex += jacobsthal_diff[k];
+        currentMainIndex += 2 * jacobsthal_diff[k];
+    }
+    while (currentPendIndex < pend.size())
+    {
+        int pendulumElement = pend[currentPendIndex];
+        int insertPos = std::upper_bound(mainChain.begin(), mainChain.end(), pendulumElement) - mainChain.begin(); // Search entire mainChain
+        mainChain.insert(mainChain.begin() + insertPos, pendulumElement);
+        currentPendIndex++;
+    }
+}
+
+void ford_johnson(std::vector<int>& data)
+{
+    //need to check memory
+    std::vector<std::pair<int, int> > pairs;
+    std::vector<int> mainChain, pend;
+
+    group_divide(data, pairs); // Step 1 & 2: Group and divide into pairs
+    recursiveSort(pairs, 0, pairs.size() - 1); // Step 3: Recursively sort by 'big' values
+
+    for (size_t i = 0; i < pairs.size(); ++i)
+    {
+        mainChain.push_back(pairs[i].second);
+        pend.push_back(pairs[i].first);
+    }
+    if(data.size() % 2 != 0)    //if the number of elements is odd, we add the last one to the end of pend, ensuring it's placed correctly no matter the value
+        pend.push_back(data.back());
+    binary_insert(mainChain, pend); //step 4 & 5: Binary insert 'small' elements
+    data = mainChain;
+}
--- C09/ex02/main.cpp
+++ C09/ex02/main.cpp
@@ -4,43 +4,53 @@
 #include <array>
 #include <unistd.h>
 #include <sys/time.h>
+#include "PmergeMe.cpp"
+#include <ctime>
 int main(int argc, char **argv)
 {
     if (argc < 2)
-    {
-        std::cout << "invalid argument" << std::endl;
         return (1);
-    }
-    std::list<int> x; //fill containers
+
+    std::list<int> x;
     std::vector<int> y;
     for (int i = 1; i < argc; i++)
     {
         long aux = strtol(argv[i], NULL, 10);
-        if(aux > 0 && aux < INT_MAX)
+        if ((aux == 0 && argv[i][0] != '0') || aux < 0 || aux > INT_MAX)
+        {
+            std::cout << "Invalid input" << std::endl;
+            return (1);
+        }
+        else if(aux >= 0 && aux <= INT_MAX)
         {
             int value = (int)aux;
             x.push_back(value);
             y.push_back(value);
         }
-        else //maybe add invalid input like letters check here
-            std::cout << "Invalid number" << std::endl;
     }
-    std::cout << "Before: ";    //print before
+
+    std::cout << "Before: ";
     for (std::vector<int>::iterator i = y.begin(); i != y.end(); i++)
         std::cout << *i << " ";
     std::cout << std::endl;
 
-    struct timeval tp;
-    gettimeofday(&tp, NULL);
-    long int begin = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+    clock_t vector_start = clock();
+    ford_johnson(y);    //vector
+    clock_t vector_end = clock();
 
-    std::sort(y.begin(), y.end());
-
-    gettimeofday(&tp, NULL);
-    long int end = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+    clock_t list_start = clock();
+   // ford_johnson(x);
+    clock_t list_end = clock();
+    
     std::cout << "after: ";
     for (std::vector<int>::iterator i = y.begin(); i != y.end(); i++)
         std::cout << *i << " ";
     std::cout << std::endl;
-    std::cout << "Time to process a range of 3000 elements with... " << end - begin << std::endl;
+
+    double vector_duration = 1000.0 * (vector_end - vector_start) / CLOCKS_PER_SEC; //time in ms
+    double list_duration = 1000.0 * (list_end - list_start) / CLOCKS_PER_SEC;       //time in ms
+    std::cout << "Time to process with std::vector " << vector_duration << " ms" << std::endl;
+    std::cout << "Time to process with std::list " << list_duration << " ms" << std::endl;
+
+    return (0);
 }
