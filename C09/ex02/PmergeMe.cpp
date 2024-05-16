669d616ff1220d750a160d921c97271d5b9fe7dd
David <david@14.local>
2024-05-16 10:47:37 +0200

    ex02 done, using std::vector and std::deque (god bless deque)

--- C09/ex02/PmergeMe.cpp
+++ C09/ex02/PmergeMe.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <list>
-#include <array>
-#include <unistd.h>
-#include <sys/time.h>
-
 void group_divide(const std::vector<int>& vector, std::vector<std::pair<int, int> >& pairs)
 {
     for (size_t i = 0; i < vector.size() - 1; i += 2)
@@ -100,4 +94,101 @@ void ford_johnson(std::vector<int>& data)
         pend.push_back(data.back());
     binary_insert(mainChain, pend); //step 4 & 5: Binary insert 'small' elements
     data = mainChain;
+}
+
+//////////////////////
+
+void group_divide(const std::deque<int>& deque, std::deque<std::pair<int, int> >& pairs) {
+    for (size_t i = 0; i < deque.size() - 1; i += 2)
+	{
+        if (deque[i] > deque[i + 1])
+        	pairs.push_back(std::make_pair(deque[i + 1], deque[i])); // step 2, swap order if left element is bigger than right element
+        else
+        	pairs.push_back(std::make_pair(deque[i], deque[i + 1]));
+    }
+}
+
+void mergePairs(std::deque<std::pair<int, int> >& pairs, int low, int mid, int high)
+{
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    std::deque<std::pair<int, int> > left(n1), right(n2);
+    for (int i = 0; i < n1; i++)
+        left[i] = pairs[low + i];
+    for (int j = 0; j < n2; j++)
+        right[j] = pairs[mid + 1 + j];
+
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2)
+	{
+        if (left[i].second <= right[j].second)
+        	pairs[k++] = left[i++];
+        else
+        	pairs[k++] = right[j++];
+        
+    }
+    while (i < n1)
+        pairs[k++] = left[i++];
+    while (j < n2)
+        pairs[k++] = right[j++];
+}
+
+void recursiveSort(std::deque<std::pair<int, int> >& pairs, int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+
+        recursiveSort(pairs, low, mid);
+        recursiveSort(pairs, mid + 1, high);
+        mergePairs(pairs, low, mid, high);
+    }
+}
+
+void    binary_insert(std::deque<int>& mainChain, std::deque<int>& pend)
+{
+    size_t  currentPendIndex = 0, currentMainIndex = 0;
+    size_t  jacobsthal_diff[] = { //https://oeis.org/A078008
+        2, 2, 6, 10, 22, 42, 86, 170, 342, 682, 1366,
+        2730, 5462, 10922, 21846, 43690, 87382, 174762, 349526, 699050,
+        1398102, 2796202, 5592406, 11184810, 22369622, 44739242, 89478486,
+        178956970, 357913942, 715827882, 1431655766};
+    
+    for (int k = 0;; ++k)
+    {
+        if (jacobsthal_diff[k] > pend.size() - currentPendIndex)
+            break;
+        for (size_t offset = 0; offset < jacobsthal_diff[k]; ++offset)
+        {
+            int pendulumElement = pend[currentPendIndex + offset];
+            int insertPos = std::upper_bound(mainChain.begin(), mainChain.begin() + currentMainIndex + offset + 1, pendulumElement) - mainChain.begin(); //std::upper_bound is binary search https://eklitzke.org/std-upper-bound-and-std-lower-bound
+            mainChain.insert(mainChain.begin() + insertPos, pendulumElement);
+        }
+        currentPendIndex += jacobsthal_diff[k];
+        currentMainIndex += 2 * jacobsthal_diff[k];
+    }
+    while (currentPendIndex < pend.size())
+    {
+        int pendulumElement = pend[currentPendIndex];
+        int insertPos = std::upper_bound(mainChain.begin(), mainChain.end(), pendulumElement) - mainChain.begin(); // Search entire mainChain
+        mainChain.insert(mainChain.begin() + insertPos, pendulumElement);
+        currentPendIndex++;
+    }
+}
+
+void ford_johnson(std::deque<int>& data) {
+    std::deque<std::pair<int, int> > pairs;
+    std::deque<int> mainChain, pend;
+
+    group_divide(data, pairs);
+    recursiveSort(pairs, 0, std::distance(pairs.begin(), pairs.end()) - 1); 
+    
+    for (size_t i = 0; i < pairs.size(); ++i)
+    {
+        mainChain.push_back(pairs[i].second);
+        pend.push_back(pairs[i].first);
+    }
+    if(data.size() % 2 != 0)    //if the number of elements is odd, we add the last one to the end of pend, ensuring it's placed correctly no matter the value
+        pend.push_back(data.back());
+    binary_insert(mainChain, pend); //step 4 & 5: Binary insert 'small' elements
+    data = mainChain;   
 }
--- C09/ex02/main.cpp
+++ C09/ex02/main.cpp
@@ -1,32 +1,28 @@
-
 #include <iostream>
-#include <list>
 #include <array>
-#include <unistd.h>
-#include <sys/time.h>
+#include <deque>
 #include "PmergeMe.cpp"
 #include <ctime>
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
         return (1);
 
-    std::list<int> x;
+    std::deque<int> x;
     std::vector<int> y;
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++) //checking for invalid input / filling both the vector and the list
     {
         long aux = strtol(argv[i], NULL, 10);
-        if ((aux == 0 && argv[i][0] != '0') || aux < 0 || aux > INT_MAX)
+        //std::cout << "valor de aux " << aux << "longitud = " << strlen(argv[i]) << std::endl;
+        if ( (aux == 0 && argv[i][0] != '0') || (strlen(argv[i]) != 1  && aux == 0) || aux < 0 || aux > INT_MAX)
         {
             std::cout << "Invalid input" << std::endl;
             return (1);
         }
-        else if(aux >= 0 && aux <= INT_MAX)
-        {
-            int value = (int)aux;
-            x.push_back(value);
-            y.push_back(value);
-        }
+        int value = (int)aux;
+        x.push_back(value);
+        y.push_back(value);
     }
 
     std::cout << "Before: ";
@@ -38,19 +34,18 @@ int main(int argc, char **argv)
     ford_johnson(y);    //vector
     clock_t vector_end = clock();
 
-    clock_t list_start = clock();
-   // ford_johnson(x);
-    clock_t list_end = clock();
+    clock_t deque_start = clock();
+    ford_johnson(x);    //deque
+    clock_t deque_end = clock();
     
     std::cout << "after: ";
-    for (std::vector<int>::iterator i = y.begin(); i != y.end(); i++)
+    for (std::deque<int>::iterator i = x.begin(); i != x.end(); i++)
         std::cout << *i << " ";
     std::cout << std::endl;
-
+    
     double vector_duration = 1000.0 * (vector_end - vector_start) / CLOCKS_PER_SEC; //time in ms
-    double list_duration = 1000.0 * (list_end - list_start) / CLOCKS_PER_SEC;       //time in ms
+    double deque = 1000.0 * (deque_end - deque_start) / CLOCKS_PER_SEC;       //time in ms
     std::cout << "Time to process with std::vector " << vector_duration << " ms" << std::endl;
-    std::cout << "Time to process with std::list " << list_duration << " ms" << std::endl;
-
+    std::cout << "Time to process with std::deque " << deque << " ms" << std::endl;
     return (0);
 }
